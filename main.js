bffc1e49514054a41b976c646ae42e91ac8fb8c3
Nguyễn Đăng Anh Đức <83125962+justakidz1402@users.noreply.github.com>
2022-07-11 21:01:07 +0700

    Update main.js

--- main.js
+++ main.js
@@ -11,3 +11,12 @@ function SignUpSuccess(){
 document.getElementById("btnSU").onclick = function(){SignUpSuccess()};
 document.getElementById("btnSU2").onclick = function(){SignUpSuccess()};
 document.getElementById("btnSU3").onclick = function(){SignUpSuccess()};
+
+$('.homeInforDown').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows:true,
+    prevArrow:'<button type="button" class="slick-prev"><</button>',
+    nextArrow:'<button type="button" class="slick-prev">></button>',
+});
