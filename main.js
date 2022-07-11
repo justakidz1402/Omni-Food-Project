0a206f4705d643e70c62c206a149a1df33b2a461
Nguyễn Đăng Anh Đức <83125962+justakidz1402@users.noreply.github.com>
2022-07-11 19:18:46 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ function ThankYou(){
 }
 
 document.getElementById("btnTY").onclick = function(){ThankYou()};
+
+function SignUpSuccess(){
+    document.getElementById("h2SU").innerHTML = '🎉🎉Congratulations! You have successfully registered.🎉🎉';
+}
+
+document.getElementById("btnSU").onclick = function(){SignUpSuccess()};
+document.getElementById("btnSU2").onclick = function(){SignUpSuccess()};
+document.getElementById("btnSU3").onclick = function(){SignUpSuccess()};
