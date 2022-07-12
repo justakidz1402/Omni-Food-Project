d915c4cf3f11d184c5938dcd3a45a84b9919a501
Nguyễn Đăng Anh Đức <83125962+justakidz1402@users.noreply.github.com>
2022-07-12 20:30:15 +0700

    Update main.js

--- main.js
+++ main.js
@@ -20,3 +20,21 @@ $('.homeInforDown').slick({
     prevArrow:'<button type="button" class="slick-prev"><</button>',
     nextArrow:'<button type="button" class="slick-prev">></button>',
 });
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
